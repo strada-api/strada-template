defe89b5c7732357c3f27dbd88038647169d7ec7
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-24 13:10:47 -0700

    deploy db

--- databases/db/drizzle.config.ts
+++ databases/db/drizzle.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import type { Config } from "drizzle-kit";
-
+console.log("SCRT_DB_URL: ", process.env.SCRT_DB_URL);
 export default {
   schema: "./src/schema/*",
   out: "./drizzle",
