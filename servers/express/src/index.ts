b113a03afbc2a6ab13d53741508385587526e3c8
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-23 18:19:50 -0700

    install pacakges

---
+++ servers/express/src/index.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", async (_, res) => {
+  try {
+    const result = await db.select().from(user);
+    res.send(JSON.stringify(result));
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.listen(3005, () => console.log(`Server ready at: http://localhost:3005`));
