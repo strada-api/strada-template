41cd149c5c4dcc7730e8ec1326b8ed1fd04b1c93
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-24 14:33:08 -0700

    local script changes

--- databases/db/docker-build.sh
+++ databases/db/docker-build.sh
@@ -16,7 +16,13 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ -z "$image_name" ]; then
-    image_name="databases-db"
+    image_name="$(basename "$(dirname "$PWD")")-$(basename "$PWD")"
+fi
+
+# Check if all required arguments are provided
+if [ -z "$image_name" ]; then
+    echo "Error: Missing required arguments."
+    usage
 fi
 
 docker build --build-arg infisical_token=$INFISICAL_TOKEN  -t $image_name .
