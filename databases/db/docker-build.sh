372737d1aef1eab3b4c577b79308a2578f8cd26c
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-24 12:57:22 -0700

    token as arg

--- databases/db/docker-build.sh
+++ databases/db/docker-build.sh
@@ -19,4 +19,4 @@ if [ -z "$image_name" ]; then
     image_name="databases-db"
 fi
 echo "build script token $INFISICAL_TOKEN"
-docker build -t $image_name .
+docker build --build-arg infisical_token=$infisical_token  -t $image_name .
