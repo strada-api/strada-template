c6f7b69c55c6ec9b824648615a5edacf985ff448
Udayan Maurya <edmauryaishu@gmail.com>
2024-05-24 17:19:45 -0700

    deploy client

--- clients/vite/src/App.tsx
+++ clients/vite/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Change 2</h1>
+      <h1>Change rush</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
