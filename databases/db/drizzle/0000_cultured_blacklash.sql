734631d2cbba9c61f0593f8513d88990fa79bcce
Udayan Maurya <edmauryaishu@gmail.com>
2024-06-14 14:13:42 -0700

    add dummy migration

---
+++ databases/db/drizzle/0000_cultured_blacklash.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS "_dummy" (
+	"id" serial NOT NULL,
+	"name" text
+);
---
+++ databases/db/src/schema/_dummy.ts
@@ -0,0 +1,6 @@
+import { serial, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
+
+export const _dummy = pgTable('_dummy', {
+  id: serial('id'),
+  name: text('name')
+});
