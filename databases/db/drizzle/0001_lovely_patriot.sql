9459a4b956b884256198b828e9e91a3322cbacfe
Udayan Maurya <edmauryaishu@gmail.com>
2024-06-19 10:15:56 -0700

    with migration

---
+++ databases/db/drizzle/0001_lovely_patriot.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "_dummy" ADD COLUMN "address" text;
--- databases/db/src/schema/_dummy.ts
+++ databases/db/src/schema/_dummy.ts
@@ -2,5 +2,6 @@ import { serial, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
 
 export const _dummy = pgTable('_dummy', {
   id: serial('id'),
-  name: text('name')
+  name: text('name'),
+  address: text('address')
 });
